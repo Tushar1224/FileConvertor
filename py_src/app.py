39346db778f0f9ad9a86deb6e7de294fd838827a
Tushar Bhattacharya <55489742+Tushar1224@users.noreply.github.com>
2025-07-27 05:22:46 +0530

    Code Finished

--- py_src/app.py
+++ py_src/app.py
@@ -29,6 +29,10 @@ def to_json(df, tgt_base_dir, ds_name, file_name):
 def file_converter(src_base_dir, tgt_base_dir, ds_name):
     schemas = json.load(open(f'{src_base_dir}/schemas.json'))
     files = glob.glob(f'{src_base_dir}/{ds_name}/part-*')
+    if len(files) == 0:
+        print(f'No files found for dataset {ds_name}')
+        return NameError(f'No files found for dataset {ds_name}')
+    print(f'Found {len(files)} files for dataset {ds_name}')
 
     for file in files:
         df = read_csv(file, schemas)
@@ -38,11 +42,19 @@ def file_converter(src_base_dir, tgt_base_dir, ds_name):
 def process_files(ds_names=None):
     src_base_dir = 'py_src/data/retail_db'
     tgt_base_dir = 'py_src/data/retail_db_json'
+    # src_base_dir = os.path.abspath('src_base_dir')
+    # tgt_base_dir = os.path.abspath('tgt_base_dir')
     schemas = json.load(open(f'{src_base_dir}/schemas.json'))
     if not ds_names:
         ds_names = schemas.keys()
     for ds_name in ds_names:
-        print(f'Processing {ds_name}')
-        file_converter(src_base_dir, tgt_base_dir, ds_name)
-
+        try:
+            print(f'Processing {ds_name}')
+            file_converter(src_base_dir, tgt_base_dir, ds_name)
+        except BameError as e:
+            print(f'Error processing {ds_name}: {e}')
+            pass
+        except Exception as e:
+            print(f'Unexpected error processing {ds_name}: {e}')
+            pass
 
--- py_src/main.py
+++ py_src/main.py
@@ -1,4 +1,12 @@
 from app import process_files
+import sys
+import json
 
-if __name__ == '__main__':      
-    process_files()
+if __name__ == '__main__':  
+    db_names = json.loads(sys.argv[1]) if len(sys.argv) > 1 else None    
+    process_files(db_names)
+
+
+# $Env:src)base_dir = 'py_src/data/retail_db'
+# $Env:tgt_base_dir = 'py_src/data/retail_db_json'
+# python main,.py '[\"orders\", \"order_items\"]'
